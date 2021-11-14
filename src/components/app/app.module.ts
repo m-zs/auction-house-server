4018a8dd9024ef04dd405fe6e518c2ade17221d8
m-zs <mzasada93@gmail.com>
2021-11-14 23:21:50 +0100

    feat: add graphql setup

--- src/components/app/app.module.ts
+++ src/components/app/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(),
+    GraphQLModule.forRoot({ autoSchemaFile: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
