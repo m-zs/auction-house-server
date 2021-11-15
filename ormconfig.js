0ec12231e29c7dc8f363a3e1e4647c8d5e69b83c
m-zs <mzasada93@gmail.com>
2021-11-15 19:07:37 +0100

    chore: change migrations path

--- ormconfig.js
+++ ormconfig.js
@@ -5,7 +5,7 @@ module.exports = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
-  migrations: ['src/migrations/*.ts'],
+  migrations: ['dist/migrations/*.js'],
   logging: true,
   ...(process.env === 'development'
     ? {
