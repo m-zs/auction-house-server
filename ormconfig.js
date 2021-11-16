7c591134c5ca69db49458d5adfd8592fa8599dc3
m-zs <mzasada93@gmail.com>
2021-11-16 19:39:17 +0100

    chore: adjust typeorm cfg, extend env

--- ormconfig.js
+++ ormconfig.js
@@ -5,7 +5,10 @@ module.exports = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
+  entities: ['dist/components/**/*.entity.js'],
   migrations: ['dist/migrations/*.js'],
+  seeds: ['dist/seeds/**/*.js'],
+  factories: ['dist/factories/**/*.js'],
   logging: true,
   ...(process.env === 'development'
     ? {
