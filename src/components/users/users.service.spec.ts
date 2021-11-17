31dbca7c75e08fe8a529c83055d0b6ff726cb465
m-zs <mzasada93@gmail.com>
2021-11-17 20:41:23 +0100

    test: fix tests

--- src/components/users/users.service.spec.ts
+++ src/components/users/users.service.spec.ts
@@ -1,18 +1,33 @@
+import {
+  // ConfigModule,
+  ConfigService,
+} from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
+
+// import { MockType } from 'src/types/testing';
+import { UserRepository } from './user.repository';
 import { UsersService } from './users.service';
 
-describe('UsersService', () => {
-  let service: UsersService;
+describe('UserService', () => {
+  let userService: UsersService;
+  // let _userRepository: MockType<UserRepository>;
+  // let _config: MockType<ConfigModule>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: UserRepository, useFactory: jest.fn() },
+        { provide: ConfigService, useFactory: jest.fn() },
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    userService = module.get<UsersService>(UsersService);
+    // _userRepository = module.get(UserRepository);
+    // _config = module.get(ConfigService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
   });
 });
