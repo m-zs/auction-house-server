8c7e33202d9a5738dcb6bd00cce77acac632fa47
m-zs <mzasada93@gmail.com>
2021-11-17 20:06:54 +0100

    feat: add global psql interceptor

---
+++ src/interceptors/psql-error.interceptor.ts
@@ -0,0 +1,41 @@
+import {
+  BadRequestException,
+  CallHandler,
+  ConflictException,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { catchError, Observable } from 'rxjs';
+
+import { DB_ERRORS, getErrorData } from 'src/utils/errors/psql';
+
+@Injectable()
+export class PsqlErrorInterceptor implements NestInterceptor {
+  intercept(_context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      catchError((error) => {
+        if (!error?.code || !error?.detail) {
+          throw error;
+        }
+
+        const { name, value } = getErrorData(error.detail);
+
+        switch (error.code) {
+          case DB_ERRORS.DUPLICATE:
+            throw new ConflictException(
+              `Value ${value} for ${name} already exists.`,
+            );
+
+          case DB_ERRORS.FOREIGN_MISSING:
+            throw new BadRequestException(
+              `Invalid value ${value} for field ${name}.`,
+            );
+
+          default:
+            throw error;
+        }
+      }),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
+import { PsqlErrorInterceptor } from './interceptors/psql-error.interceptor';
 import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalInterceptors(new PsqlErrorInterceptor());
   app.useGlobalInterceptors(new TransformInterceptor());
 
   await app.listen(3000);
