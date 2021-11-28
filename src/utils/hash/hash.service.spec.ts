49b3dce1307a79fb2b2e920ef8317b657236096c
m-zs <mzasada93@gmail.com>
2021-11-28 19:33:57 +0100

    test: add hash service tests

---
+++ src/utils/hash/hash.service.spec.ts
@@ -0,0 +1,62 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigService } from '@nestjs/config';
+import * as bcrypt from 'bcrypt';
+
+import { HashService } from './hash.service';
+import { MockType } from 'src/types/testing';
+
+jest.mock('bcrypt', () => ({
+  hash: jest.fn(),
+}));
+
+describe('HashService', () => {
+  let hashService: HashService;
+  let configService: MockType<ConfigService>;
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        HashService,
+        {
+          provide: ConfigService,
+          useFactory: jest.fn(() => ({ get: jest.fn() })),
+        },
+      ],
+    }).compile();
+
+    hashService = module.get<HashService>(HashService);
+    configService = module.get(ConfigService);
+  });
+
+  it('should call dependencies with default arguments and return string', async () => {
+    const string = 'string';
+    const saltRounds = 1;
+    const hashResult = 'hashResult';
+
+    configService.get.mockReturnValueOnce(saltRounds);
+    (bcrypt.hash as jest.Mock).mockReturnValueOnce(hashResult);
+
+    const result = await hashService.hash(string);
+
+    expect(bcrypt.hash).toHaveBeenCalledTimes(1);
+    expect(bcrypt.hash).toHaveBeenCalledWith(string, saltRounds);
+    expect(result).toBe(hashResult);
+  });
+
+  it('should call dependencies with provided arguments and return string', async () => {
+    const string = 'string';
+    const saltRounds = 100;
+    const hashResult = 'hashResult';
+
+    configService.get.mockReturnValueOnce(saltRounds);
+    (bcrypt.hash as jest.Mock).mockReturnValueOnce(hashResult);
+
+    const result = await hashService.hash(string, saltRounds);
+
+    expect(bcrypt.hash).toHaveBeenCalledTimes(1);
+    expect(bcrypt.hash).toHaveBeenCalledWith(string, saltRounds);
+    expect(result).toBe(hashResult);
+  });
+});
