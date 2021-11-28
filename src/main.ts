6227d6406e7832d57197940b3f13c85344771ed1
m-zs <mzasada93@gmail.com>
2021-11-28 15:53:19 +0100

    feat: add cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 import { PsqlErrorInterceptor } from './interceptors/psql-error.interceptor';
@@ -7,6 +8,7 @@ import { TransformInterceptor } from './interceptors/transform.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cookieParser());
   app.enableCors({ origin: true, credentials: true });
   app.useGlobalInterceptors(new PsqlErrorInterceptor());
   app.useGlobalInterceptors(new TransformInterceptor());
