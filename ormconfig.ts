2be537e4e9eb9247ae7919999f0a02bd79785e41
m-zs <mzasada93@gmail.com>
2021-12-03 13:29:08 +0100

    chore: adjust db config and commands

---
+++ ormconfig.ts
@@ -0,0 +1,10 @@
+import { ConfigModule } from '@nestjs/config';
+
+import dbConfiguration from './src/config/db.config';
+
+ConfigModule.forRoot({
+  isGlobal: true,
+  load: [dbConfiguration],
+});
+
+export default dbConfiguration();
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 
+import dbConfiguration from './config/db.config';
 import { UsersModule } from './components/users/users.module';
 import { AuthModule } from './components/auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [dbConfiguration],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.get('database'),
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
       context: ({ req, res }) => ({ req, res }),
