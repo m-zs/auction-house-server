9c38bcea19630ad8be3b28d07607aebd6dbcf738
m-zs <mzasada93@gmail.com>
2021-12-07 22:45:40 +0100

    test: add auth resolver tests

--- src/components/auth/auth.resolver.spec.ts
+++ src/components/auth/auth.resolver.spec.ts
@@ -1,20 +1,108 @@
-// import { Test, TestingModule } from '@nestjs/testing';
+import { Test, TestingModule } from '@nestjs/testing';
+import { User } from 'components/users/entities/user.entity';
+import * as faker from 'faker';
 
-// import { AuthResolver } from './auth.resolver';
-// import { AuthService } from './auth.service';
+import { GlobalContext } from 'types/context';
+import { MockType } from 'types/testing';
+import { COOKIE_NAME } from './auth.const';
+import { AuthResolver } from './auth.resolver';
+import { AuthService } from './auth.service';
 
 describe('AuthResolver', () => {
-  // let resolver: AuthResolver;
+  let authResolver: AuthResolver;
+  let authService: MockType<AuthService>;
 
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     providers: [AuthResolver, AuthService],
-  //   }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AuthResolver,
+        {
+          provide: AuthService,
+          useFactory: () => ({
+            signIn: jest.fn(),
+            signOut: jest.fn(),
+            refresh: jest.fn(),
+          }),
+        },
+      ],
+    }).compile();
 
-  //   resolver = module.get<AuthResolver>(AuthResolver);
-  // });
+    authResolver = module.get<AuthResolver>(AuthResolver);
+    authService = module.get(AuthService);
+  });
 
-  it.skip('should be defined', () => {
-    // expect(resolver).toBeDefined();
+  describe('signIn', () => {
+    it('should call auth service, setup cookie and return token', async () => {
+      const credentials = {
+        username: faker.internet.userName(),
+        password: faker.internet.password(),
+      };
+      const ctx = { res: { cookie: jest.fn() } };
+      const authServiceResponse = {
+        token: 'token',
+        refreshToken: 'refreshToken',
+      };
+
+      authService.signIn.mockResolvedValueOnce(authServiceResponse);
+
+      const result = await authResolver.signIn(
+        credentials,
+        ctx as unknown as GlobalContext,
+      );
+
+      expect(ctx.res.cookie).toHaveBeenCalledWith(
+        COOKIE_NAME,
+        authServiceResponse.refreshToken,
+        { httpOnly: true },
+      );
+      expect(result).toStrictEqual({ token: authServiceResponse.token });
+    });
+  });
+
+  describe('signOut', () => {
+    it('should call auth service and return boolean', async () => {
+      const ctx = { res: { clearCookie: jest.fn() } };
+      const user = {} as User;
+      const authServiceResponse = false;
+
+      authService.signOut.mockResolvedValueOnce(authServiceResponse);
+
+      const result = await authResolver.signOut(
+        ctx as unknown as GlobalContext,
+        user,
+      );
+
+      expect(ctx.res.clearCookie).toHaveBeenCalledTimes(0);
+      expect(result).toBe(authServiceResponse);
+    });
+
+    it('should call auth service, clearCokkie and return boolean', async () => {
+      const ctx = { res: { clearCookie: jest.fn() } };
+      const user = {} as User;
+      const authServiceResponse = true;
+
+      authService.signOut.mockResolvedValueOnce(authServiceResponse);
+
+      const result = await authResolver.signOut(
+        ctx as unknown as GlobalContext,
+        user,
+      );
+
+      expect(ctx.res.clearCookie).toHaveBeenCalledTimes(1);
+      expect(result).toBe(authServiceResponse);
+    });
+  });
+
+  describe('refresh', () => {
+    it('should call auth service and return token payload', () => {
+      const user = {} as User;
+      const authServiceResponse = { token: 'token' };
+
+      authService.refresh.mockReturnValueOnce(authServiceResponse);
+
+      const result = authResolver.refresh(user);
+
+      expect(result).toStrictEqual(authServiceResponse);
+    });
   });
 });
