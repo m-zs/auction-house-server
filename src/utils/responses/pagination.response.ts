c558b638d7d24b0270b52699c426d77fca296def
m-zs <mzasada93@gmail.com>
2022-01-13 17:45:50 +0100

    fix: move out Meta definition outside fn

--- src/utils/responses/pagination.response.ts
+++ src/utils/responses/pagination.response.ts
@@ -1,25 +1,25 @@
 import { Type } from '@nestjs/common';
 import { Field, Int, ObjectType } from '@nestjs/graphql';
 
+@ObjectType()
+class Meta {
+  @Field(() => Int)
+  totalItems: number;
+
+  @Field(() => Int)
+  itemCount: number;
+
+  @Field(() => Int)
+  itemsPerPage: number;
+
+  @Field(() => Int)
+  totalPages: number;
+
+  @Field(() => Int)
+  currentPage: number;
+}
+
 export function paginatedResponse<T>(type: Type<T>): any {
-  @ObjectType()
-  class Meta {
-    @Field(() => Int)
-    totalItems: number;
-
-    @Field(() => Int)
-    itemCount: number;
-
-    @Field(() => Int)
-    itemsPerPage: number;
-
-    @Field(() => Int)
-    totalPages: number;
-
-    @Field(() => Int)
-    currentPage: number;
-  }
-
   @ObjectType()
   class PaginatedResponse {
     @Field(() => [type])
