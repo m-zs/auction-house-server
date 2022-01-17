7bb4203d5590d71cdd8fe4c61368cfcf36a0d60c
m-zs <mzasada93@gmail.com>
2022-01-17 11:06:05 +0100

    refactor: adjust user seeding

--- src/factories/user.factory.ts
+++ src/factories/user.factory.ts
@@ -1,20 +1,35 @@
 import { define } from 'typeorm-seeding';
 import * as bcrypt from 'bcrypt';
 
-import { User } from '../components/users/entities/user.entity';
-import { USER_ROLE, USER_STATUS } from '../components/users/user.types';
+import { User } from 'components/users/entities/user.entity';
+import { USER_ROLE, USER_STATUS } from 'components/users/user.types';
 
-define(User, (faker) => {
-  const user = new User();
+define(
+  User,
+  (
+    faker,
+    {
+      role = USER_ROLE.USER,
+      status = USER_STATUS.ACTIVE,
+      password = faker.internet.password(),
+      username = faker.internet.userName(),
+      email = faker.internet.email(),
+    }: {
+      role?: USER_ROLE;
+      status?: USER_STATUS.ACTIVE;
+      password?: string;
+      username?: string;
+      email?: string;
+    } = {},
+  ) => {
+    const user = new User();
 
-  user.username = faker.internet.userName();
-  user.email = faker.internet.email();
-  user.password = bcrypt.hashSync(
-    faker.internet.password(),
-    Number(process.env.SALT_ROUNDS),
-  );
-  user.status = USER_STATUS.ACTIVE;
-  user.role = USER_ROLE.USER;
+    user.username = username;
+    user.email = email;
+    user.password = bcrypt.hashSync(password, Number(process.env.SALT_ROUNDS));
+    user.status = status;
+    user.role = role;
 
-  return user;
-});
+    return user;
+  },
+);
--- src/seeds/0-users.seed.ts
+++ src/seeds/0-users.seed.ts
@@ -1,6 +1,6 @@
 import { Factory, Seeder } from 'typeorm-seeding';
 
-import { User } from '../components/users/entities/user.entity';
+import { User } from 'components/users/entities/user.entity';
 
 export default class CreateUsers implements Seeder {
   public async run(factory: Factory): Promise<void> {
